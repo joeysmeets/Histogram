2299b32002c7c8a153f79cf60d3dd5ca49ddb8b1
Joey Smeets <joeysmeetsss@gmail.com>
2021-05-07 13:57:41 +0200

    I added the final version that made the code work. TestParser is not removed and everything is in the main class Histogram.

--- src/Histogram.java
+++ src/Histogram.java
@@ -1,39 +1,72 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
- /**
-This program reads a file with numbers, and writes the numbers to another
-file, lined up in a column and followed by their total.
- */
-public class Histogram
-{
+
+public class Histogram {
 	public static void main(String[] args) throws FileNotFoundException
-	{
-	// Prompt for the input and output file names							
-		Scanner in = new Scanner("/Users/joey/Documents/HTW/2nd Semester/Info 2/Projects/Histogram/Input.txt");
-		System.out.print("Input file: Input.txt");
-		String inputFileName = in.next();
-		System.out.print("Output file: Output.txt");
-		String outputFileName = in.next();
-		
-	// Construct the Scanner and PrintWriter objects for reading and writing
-		File inputFile = new File(inputFileName);
+    {
+		File inputFile = new File ("C:\\Users\\alexj\\Desktop\\HTW\\#2 Module_sem2\\Labs\\Lab4\\TextFiles\\Lorem3.txt");
 		Scanner in = new Scanner(inputFile);
-		PrintWriter out = new PrintWriter(outputFileName);
-	
-	// Read the input and write the output 
+		in.useDelimiter("");
+		
+		Map<Character, Integer> charMap = new HashMap<Character, Integer>();
+		
+		//PrintWriter
+		PrintWriter out = new PrintWriter("C:\\Users\\alexj\\Desktop\\HTW\\#2 Module_sem2\\Labs\\Lab4\\TextFiles\\Output.txt");		
+		
+		//Put Alphabet into Hashmap
+		for(char cha= 'A'; cha<='Z'; cha++) {
+			charMap.put(Character.valueOf(cha), 0);
+		}
+		
 		while (in.hasNext()) {
-			char ch = in.next().charAt(0);
-			if (isTheSame(ch)) {
-				vowels++;
+			String character = in.next();
+			
+			
+			character = character.replaceAll("\\n", " ");
+			character = character.replaceAll("\\r", " ");
+			
+			String bigCh = character.toUpperCase(); //Makes all letters to capital letters
+			
+			char ch = bigCh.charAt(0); //String -> Char
+			
+					
+					if (charMap.containsKey(ch))
+		    		{charMap.put(ch, charMap.get(ch) + 1);
+					
+		    		}
+					else {
+		    		charMap.put(ch, 1);
+					}
+		}
+		//find Max
+		int max = Collections.max(charMap.values());
+				
+		//scale regarding to max 2.0
+		for(Entry<Character, Integer> entry : charMap.entrySet()) {
+			
+			//getting the %
+			int proz=((entry.getValue()*100)/max);
+						
+			//replace old value with new ones
+			charMap.put(entry.getKey(), proz);
+												
+			//print * (histogram)
+			out.printf(entry.getKey()+": ");
+			for(int i=1; i<=proz; i++) {
+				if(i%10==0) {out.printf("*|");
+				} else	out.printf("*");
 			}
+			out.printf("\n");
 		}
-		try {
-			Map<Character, Integer> dict = TextParser.parseFile(selectedFile);
-			generateHistogram(dict, true);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+		//System.out.println(charMap);
+		in.close();
+		out.close();
+    }
+	
 }
